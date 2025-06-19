0a34b8f8ca158c72b9215ea0bb615d241f0716c4
GalatanuM <101355903+GalatanuM@users.noreply.github.com>
2025-06-19 23:12:26 +0300

    VBUS sensing to detect overvoltage

--- Proiect/SimulareVolan/Core/Src/stm32f4xx_hal_msp.c
+++ Proiect/SimulareVolan/Core/Src/stm32f4xx_hal_msp.c
@@ -97,13 +97,20 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
     __HAL_RCC_ADC1_CLK_ENABLE();
 
     __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
     /**ADC1 GPIO Configuration
     PC0     ------> ADC1_IN10
+    PA6     ------> ADC1_IN6
     */
     GPIO_InitStruct.Pin = GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* USER CODE BEGIN ADC1_MspInit 1 */
 
@@ -174,8 +181,11 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
 
     /**ADC1 GPIO Configuration
     PC0     ------> ADC1_IN10
+    PA6     ------> ADC1_IN6
     */
     HAL_GPIO_DeInit(GPIOC, GPIO_PIN_0);
+
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_6);
 
     /* USER CODE BEGIN ADC1_MspDeInit 1 */
 
